6987a87d3330a228a3d26a4ee10bec29084517c5
Fatema <fatemaemadd@gmail.com>
2025-11-15 09:14:03 +0200

    Create J - Basic Data Types.cpp

---
+++ J - Basic Data Types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int in;
+long long lng;
+char ch;
+float flt;
+double d;
+
+//Enter int , long long , char , float , double 
+cin >>in>>lng>>ch>>flt>>d;
+cout << in <<endl<< lng <<endl<< ch <<endl<< flt <<endl<< d;
+
+return 0;
+}
