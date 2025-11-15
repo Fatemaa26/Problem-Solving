2074215a29a46bcc9883b5740ded1ccc33c5d814
Fatema <fatemaemadd@gmail.com>
2025-11-15 09:12:35 +0200

    Create K - Simple Calculator.cpp

---
+++ K - Simple Calculator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+long long X, Y;
+
+//Enter X and Y
+cin>>X>>Y;
+//Calculate summation , multiplication and subtraction of X and Y
+cout<<X<<" + "<<Y<<" = "<<X+Y<<endl;
+cout<<X<<" * "<<Y<<" = "<<X*Y<<endl;
+cout<<X<<" - "<<Y<<" = "<<X-Y ;
+return 0;
+}
