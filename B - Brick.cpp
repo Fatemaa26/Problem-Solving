573afa708f87733bcd2eee5b9825e30bc14eac85
Fatema <fatemaemadd@gmail.com>
2025-11-15 09:16:33 +0200

    Create B - Brick.cpp

---
+++ B - Brick.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int N,W;
+int max_number;
+
+//Enter N and W
+cin>>N>>W;
+//calculate maximum number 
+max_number=N/W;
+cout<<max_number<<endl;
+return 0;
+}
