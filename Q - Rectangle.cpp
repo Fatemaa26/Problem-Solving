47451dd1a8859e08950cfcbaec62de16a954a043
Fatema <fatemaemadd@gmail.com>
2025-11-15 09:15:59 +0200

    Create Q - Rectangle.cpp

---
+++ Q - Rectangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a,b;
+int area,perimeter;
+
+//Enter a and b
+cin>>a>> b;
+//calculate area and perimeter 
+area=a*b;
+perimeter=2*(a+b);
+cout<<area<<" "<< perimeter<<endl;
+return 0;
+}
